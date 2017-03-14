9c1a9cda176d14a1dd31c60c411c26e3d4fb92cf
Chris <chrispdunne@gmail.com>
2017-03-14 19:50:00 +0000

    a s d keys triggering chat

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/audio/drip.wav');
 });
 
 io.on('connection', function(socket){

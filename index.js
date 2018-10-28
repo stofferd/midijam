b720bcb405f35875e8e57ec60fde93ca4358c324
Chris <chrispdunne@gmail.com>
2018-10-28 22:07:36 +0000

    latest commit?!

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var fs = require('fs');
+var ejs = require('ejs');
+var path = require('path')
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
-  res.sendFile(__dirname + '/audio/drip.wav');
 });
+
+// app.get('/audio/', function(req, res){
+//   res.sendFile(__dirname + '/drip.wav');
+// });
+
+
+var filepath = path.join(__dirname, 'drip.wav');
+
+app.get('/audio', function(req, res){
+    res.set({'Content-Type': 'audio/mpeg'});
+    var readStream = fs.createReadStream(filepath);
+    readStream.pipe(res);
+})
+
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
